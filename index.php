1eadecc30005b1301df8b6dc6278a70a03ebaee2
amitthink360 <amit.think360@gmail.com>
2022-09-01 00:52:36 +0530

    Update code

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="format-detection" content="telephone=no" />
+
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+
+        <link rel="canonical" href="https://www.contaminatedwateratcamplejeune.com/" />
+
+        <link rel="shortcut icon" href="https://camplejeuneresources.com/jmp/favicon.png" type="image/ico" />
+        <title>Camp Lejuene Water Contamination - Free Case Evaluation</title>
+
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="" />
+        <meta property="og:description" content="" />
+        <meta property="og:site_name" content="" />
+        <meta property="og:url" content="https://www.contaminatedwateratcamplejeune.com/" />
+
+        <link rel="apple-touch-icon" sizes="57x57" href="https://camplejeuneresources.com/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="https://camplejeuneresources.com/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="https://camplejeuneresources.com/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="https://camplejeuneresources.com/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="https://camplejeuneresources.com/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="https://camplejeuneresources.com/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="https://camplejeuneresources.com/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="https://camplejeuneresources.com/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="https://camplejeuneresources.com/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="https://camplejeuneresources.com/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="https://camplejeuneresources.com/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="https://camplejeuneresources.com/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="https://camplejeuneresources.com/favicon-16x16.png" />
+        <link rel="manifest" href="https://camplejeuneresources.com/manifest.json" />
+
+        <link type="text/css" rel="stylesheet" href="./css/extra.css" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff" />
+    </head>
+    <body id="landing-page">
+        <main>
+            <div class="section-inner section-fit section-relative"></div>
+            <div class="section-block">
+                <div class="section-inner section-fit section-relative" style="display: flex; flex-direction: column; justify-content: center; align-content: center; align-items: center;">
+                    <div style="display: flex; align-items: center; justify-content: center; max-width: 500px;">
+                        <img style="width: 100%;" src="./images/jmp2logo.png" />
+                    </div>
+                    <div style="display: block; text-align: center; padding: 10px 10px 0 10px; color: grey; max-width: 500px;">Welcome back, please complete the following survey to see the new benefits and resources for your area:</div>
+                    <div style="-webkit-box-shadow: 9px 10px 14px 4px rgba(0, 0, 0, 0.47);box-shadow: 9px 10px 14px 4px rgba(0, 0, 0, 0.47);display: block;padding: 1rem;margin: 30px;text-align: center;border-radius: 10px;background-color: white;max-width: 500px;">
+                        <div style="display: block; padding: 10px; margin: 0px; text-align: center; border-radius: 0px; color: rgb(7, 54, 80);">
+                            <h1 style="font-size: 1.4rem; font-weight: bolder;">Camp Lejeune Water Contamination Lawsuit - Get Compensated!</h1>
+                        </div>
+                        <br />
+
+                        <p style="font-size: 0.95rem; font-weight: bold; color: rgb(13, 50, 69);">Over a million poeple might have been exposed to the Camp Lejeune contaminated drinking water - Click Below for Compensation</p>
+
+                        <p><br /></p>
+                        <a href="https://contaminatedwateratcamplejeune.com/lp2.php?affid=34727&amp;reqid=140447077&amp;s1=MOBILE_{var1}&amp;s2=&amp;s3={clickid}">
+                            <div style="display: block; text-align: center; color: white; font-size: 16px; font-weight: bold; border-radius: 16px; width: 75%; padding: 10px 40px; margin: 10px auto;">Yes</div>
+                        </a>
+                        <a><div id="No" style="display: block; color: white; text-align: center; font-size: 16px; font-weight: bold; border-radius: 16px; width: 75%; padding: 10px 40px; margin: 10px auto;">No</div></a>
+                        <p><br /></p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
